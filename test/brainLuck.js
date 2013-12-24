e10e6d1bd5e0262a9a8c278de2ca56f803749a65
OpenShift guest <nico.franza@gmail.com>
2013-12-24 06:24:14 -0500

    Added fix for second test passing

--- test/brainLuck.js
+++ test/brainLuck.js
@@ -1,10 +1,8 @@
 var Test = require("kata-test-framework-js/index").Test;
 var brainLuck = require("../lib/brainLuck");
 
-// brainLuck(',+[-.,+]', 'Codewars');
-
 // Echo until byte(255) encountred
-// Test.assertEquals(brainLuck(',+[-.,+]','Codewarsÿ'), 'Codewars');
+Test.assertEquals(brainLuck(',+[-.,+]','Codewars' + String.fromCharCode(255)), 'Codewars');
 
 // // Echo until byte(0) encountred
 Test.assertEquals(brainLuck(',[.[-],]','Codewars'), 'Codewars');
