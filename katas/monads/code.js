fbeddaaef45a04549a47bf0819a176e886baa3f3
franza <nico.franza@gmail.com>
2014-05-30 16:45:21 -0400

    all tests passed

--- katas/monads/code.js
+++ katas/monads/code.js
@@ -1,9 +1,7 @@
-/////////////////////////////////////////////////////////////////
 function Maybe () {
   Object.freeze(this);
 }
 
-/////////////////////////////////////////////////////////////////
 function Just (x) {
   this.toString = function () { return "Just " + x.toString(); };
   this.just = x;
@@ -12,7 +10,6 @@ function Just (x) {
 Just.prototype = new Maybe();
 Just.prototype.constructor = Just;
 
-/////////////////////////////////////////////////////////////////
 function Nothing () {
   this.toString = function () { return "Nothing"; };
   Object.freeze(this);
@@ -20,24 +17,31 @@ function Nothing () {
 Nothing.prototype = new Maybe();
 Nothing.prototype.constructor = Nothing;
 
-/////////////////////////////////////////////////////////////////
 Maybe.unit = function (x) {
   return new Just(x);
 };
 
 Maybe.bind = function (f) {
-  // given a function from a value to a Maybe return a function from a Maybe to a Maybe
+  return function (maybe) {
+    return maybe instanceof Nothing ? new Nothing() : f(maybe.just);
+  };
 };
 
 Maybe.lift = function (f) {
-  // given a function from value to value, return a function from value to Maybe
-  // if f throws an exception, (lift f) should return a Nothing
+  return function (value) {
+    try {
+      return new Just(f(value));
+    } catch (e) {
+      return new Nothing();
+    }
+  };
 };
 
 Maybe.do = function(m) {
   var fns = Array.prototype.slice.call(arguments, 1);
-  // given a Maybe m and some functions fns, run m into the first function,
-  // pass that result to the second function, etc. and return the last result
+  return fns.reduce(function (m, fn) {
+    return m instanceof Nothing ? new Nothing() : fn(m.just);
+  }, m);
 };
 
 exports.Maybe = Maybe;
--- katas/monads/test.js
+++ katas/monads/test.js
@@ -9,3 +9,40 @@ var just5 = Maybe.unit(5);
 
 Test.expect(just5 instanceof Just === true, 'Unit should return Just');
 Test.expect(just5.just == 5, 'Unit should wrap an object in monad');
+
+function parseInt2(x) {
+  var int = parseInt(x);
+  if (isNaN(x)) {
+    return new Nothing();
+  }
+  return new Just(int);
+}
+
+var boundParseInt2 = Maybe.bind(parseInt2);
+var just5Str = new Just('5');
+
+Test.expect(boundParseInt2(just5Str) instanceof Just === true, 'Function result should be a monad');
+Test.expect(boundParseInt2(new Nothing()) instanceof Nothing === true, 'Bound function that takes Nothing returns Nothing');
+
+function parseInt3(x) {
+  if (x == 3) {
+    throw new Error();
+  }
+  return parseInt(x);
+}
+
+var liftParseInt = Maybe.lift(parseInt3);
+
+Test.expect(liftParseInt(4) instanceof Maybe === true, 'Lifted function result should be a monad');
+Test.expect(liftParseInt(3) instanceof Nothing === true, 'If original function throws error, lifted should return Nothing');
+
+function add(a, b) {
+  return a + b;
+}
+
+var a1 = Maybe.lift(add.bind(null, '1'));
+var a2 = Maybe.lift(add.bind(null, '2'));
+var a3 = Maybe.lift(add.bind(null, '3'));
+
+Test.expect(Maybe.do(Maybe.unit(''), a1, a2, a3).just === '321', '@do should pass initial value to each of functions');
+Test.expect(Maybe.do(new Nothing(), a1, a2, a3) instanceof Nothing, '@do of Nothing is Nothing');
